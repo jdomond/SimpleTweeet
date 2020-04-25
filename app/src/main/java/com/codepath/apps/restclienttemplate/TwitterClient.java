282e94e9813b1caa2affb075021ad2ed6ea4680d
Domond Josue <jdomond18@gmail.com>
2020-04-24 22:16:00 -0400

    readMe commit

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -26,7 +26,7 @@ public class TwitterClient extends OAuthBaseClient {
     public static final String REST_URL = "https://api.twitter.com/1.1";
 	//public static final String REST_URL = "https://api.twitter.com/services"; // Change this, base API URL
 	public static final String REST_CONSUMER_KEY =BuildConfig.CONSUMER_KEY;    // Change this inside apikey.properties
-	public static final String REST_CONSUMER_SECRET =BuildConfig.CONSUMER_SECRET; ; // Change this inside apikey.properties
+	public static final String REST_CONSUMER_SECRET =BuildConfig.CONSUMER_SECRET; // Change this inside apikey.properties
 
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
 	public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
